4f5dadd457b8d08f04240cb4ae3277b460db6f43
georgianamadalinacalugareanu <79469386+georgianamadalinacalugareanu@users.noreply.github.com>
2021-04-15 10:30:42 +0300

    composite

---
+++ sem8/src/composite/Main.java
@@ -0,0 +1,30 @@
+package composite;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sectiune sectiuneBauturi=new Sectiune("Bauturi");
+		Sectiune sectiuneDesert=new Sectiune("Desert");
+		Item itemFrappe=new Item("Frappe");
+		Item itemCafea=new Item("Cafea");
+		Item itemEcler=new Item("Ecler");
+		
+		sectiuneBauturi.adaugaComponenta(itemCafea);
+		sectiuneBauturi.adaugaComponenta(itemFrappe);
+		sectiuneDesert.adaugaComponenta(itemEcler);
+		
+		Sectiune meniu=new Sectiune("Meniu");
+		meniu.adaugaComponenta(sectiuneDesert);
+		meniu.adaugaComponenta(sectiuneBauturi);
+		
+		meniu.afiseazaDescriere();
+		
+		sectiuneBauturi.stergeComponenta(itemFrappe);
+		sectiuneDesert.adaugaComponenta(itemFrappe);
+		
+		meniu.afiseazaDescriere();
+
+	}
+
+}
