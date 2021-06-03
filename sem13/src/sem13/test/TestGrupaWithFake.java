a014b0340e7e90a04cfc56ba7a3e0442fb551384
georgianamadalinacalugareanu <79469386+georgianamadalinacalugareanu@users.noreply.github.com>
2021-06-03 12:49:27 +0300

    fake

---
+++ sem13/src/sem13/test/TestGrupaWithFake.java
@@ -0,0 +1,32 @@
+package sem13.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import sem13.clase.Grupa;
+import sem13.clase.IStudent;
+import sem13.dubluri.StudentFake;
+
+public class TestGrupaWithFake {
+
+	@Test
+	public void testPromovabilitateRight() {
+		Grupa grupa=new Grupa(1076);
+		for(int i=0;i<8;i++) {
+			StudentFake sf=new StudentFake();
+			sf.setValoareAreRestante(false);
+			grupa.adaugaStudent(sf);
+			
+
+		}
+		for(int i=0;i<2;i++) {
+			StudentFake sf=new StudentFake();
+			sf.setValoareAreRestante(true);
+			grupa.adaugaStudent(sf);
+		}
+	assertEquals(0.8,grupa.getPromovabilitate(),0.01);
+	
+	}
+
+}
